907e4d7390516ff4bd62ae0a43f1ee94c078ba3d
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2024-09-20 15:41:27 -0500

    aaa

--- src/ngram.js
+++ src/ngram.js
@@ -1,17 +1,32 @@
 import { readFileSync, writeFileSync, existsSync } from 'fs'
-import {  BPE } from './bpe.js'
+import { BPE } from './bpe.js'
 import { frequencies } from './shared.js'
 
+// clojure util
+const assoc_in = (map, keys, val) => { 
+    keys.reduce((acc, next, idx) => {
+        if(idx == keys.length - 1) {
+            acc[next] = val
+        } else {
+            acc[next] = {}
+        }
+        return acc[next]
+    }, map)
+    return map
+}
+
+const deep_has = (map, keys) => {
+    
+}
 
 function make_ngrams (arr, n) {
     const ngrams = []
     for (let i = 0; i < arr.length-n+1; i++) {
         let ngram=""
-        for (let j = 0; j < n-1; j++) {
+        for (let j = 0; j < n; j++) {
             ngram+=arr[i+j]
+            ngram+="%"
         }
-        ngram+="%"
-        ngram+=arr[i+n-1]
         ngrams.push( ngram )
    }
     return ngrams
@@ -28,51 +43,52 @@ class Ngram {
     _load_vocabulary() {
         if (existsSync(this.model_name)) {
             this.trained = true
-            return new Map(Object.entries(JSON.parse(readFileSync(this.model_name, { encoding: 'utf8' }))))
+            return new JSON.parse(readFileSync(this.model_name, { encoding: 'utf8' }))
         }else{
             this.trained = false 
-            return new Map()
+            return {};
         }
     }
     train(corpus) {
         const [tokens, tids] = this.tokenizer.tokenize(corpus)
-        console.log(tokens)
         const ngrams = make_ngrams(tokens, this.n+1)
         const ngramfrequences = frequencies(ngrams)
         for (const ngram of ngrams) {
             const freq = ngramfrequences.get(ngram) 
             if(freq) {
-                const [first, last] = ngram.split('%')
+                const tokens = ngram.split('%')
+                const previous = tokens.slice(0,this.n)
+                const last = tokens.at(-1)
+                
+                assoc_in(this.model, previous, [last]) 
                 //console.log(first, last)
-                if(!this.model.has(first)) {
-                    this.model.set(first, [])
-                } 
-                this.model.get(first).push(last)
+                if(!this.model[first]) {
+                    this.model[first] = []
+                }
+                this.model[first].push(last)
                
             }
         }
         writeFileSync(this.model_name,
-            JSON.stringify(Object.fromEntries(this.model.entries())), { 'encoding': 'utf8' }, 4)
+            JSON.stringify(this.model), { 'encoding': 'utf8' }, 4)
     }
 
     predict(words) {
         const [tokens] = this.tokenizer.tokenize(words)
         let out = ""
         let next = tokens.slice(tokens.length-this.n).join("")
-        out += next
-        const keys = Array.from(this.model.keys())
+        out += tokens.slice(tokens.length-this.n).join(" ")
+        const keys = Array.from(Object.keys(this.model))
         for(let i = 0; i < 100; i++) {
-            console.log(next)
             const choices = this.model.get(next)
             if(!choices) {
-                console.log("random")
                 const ran = keys[Math.floor(Math.random() * keys.length)]
                 next = ran
-                out += ran
+                out +=  ran + " "
             } else {
                 const choice = choices[Math.floor(Math.random() * choices.length)]
                 next = choice
-                out += choice
+                out +=  choice + " "
             }
         }
         return out
@@ -90,7 +106,8 @@ const ngram = new Ngram('woodchuck.json', 2, new WSTokenizer())
 //ngram.train(readFileSync('moby.txt', { encoding: 'utf8' }))
 
 
-console.log(ngram.train("How much wood could a woodchuck chuck?"))
+ngram.train(readFileSync('moby.txt', { encoding: 'utf8' }))
+console.log(ngram.predict("Moby Dick"))
 
 
 
